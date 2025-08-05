b2af2970241bf01d63a9bdcaeee1b7604ddece34
Thiago Pereira <thiagopnascimento11@gmail.com>
2025-08-05 19:43:29 -0300

    incluindo configurações no projeto

--- src/main/java/com/devsuperior/dscommerce/respositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devsuperior.dscommerce.respositories;
-
-import com.devsuperior.dscommerce.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,17 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$82ysEuGFE7KQKslH.J5sEusrJhlhjWN8nVV3g6qYGoXAb.l28DPgK');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$82ysEuGFE7KQKslH.J5sEusrJhlhjWN8nVV3g6qYGoXAb.l28DPgK');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+INSERT INTO tb_product (name) VALUES ('TV');
+INSERT INTO tb_product (name) VALUES ('Computer');
+
+
 INSERT INTO tb_category(name) VALUES ('Livros');
 INSERT INTO tb_category(name) VALUES ('Eletrônicos');
 INSERT INTO tb_category(name) VALUES ('Computadores');
