54b1b773f2fd39a44aac7d03c582293aef2f59f2
Thiago Pereira <thiagopnascimento11@gmail.com>
2025-08-12 17:29:54 -0300

    GET user logged

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscommerce.controllers;
+
+import com.devsuperior.dscommerce.dto.UserDTO;
+import com.devsuperior.dscommerce.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController //Ela configura para que quando a aplicação rodar, o que estiver implementado estará respondendo pela web
+@RequestMapping(value = "/users") //Rota
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDTO> getMe() {
+        UserDTO dto = service.getMe();
+        return ResponseEntity.ok(dto);
+    }
+
+
+
+}
